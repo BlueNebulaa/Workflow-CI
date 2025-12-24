6589663b14fb87c1856d1676de7021fe8eed93e3
BlueNebulaa <nbil.ibrahim04@gmail.com>
2025-12-24 17:18:11 +0700

    fixing modeling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,19 +1,13 @@
-import pandas as pd
 import mlflow
-import mlflow.sklearn
-import dagshub
-from sklearn.model_selection import train_test_split, GridSearchCV
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+import random
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-# TETAP LOCALHOST (default)
-dagshub.init(
-    repo_owner="BlueNebulaa",
-    repo_name="SML_Muhammad-Nabil-Ibrahim",
-    mlflow=True
-)
-
-df = pd.read_csv("../data_clean/data_clean.csv")
+df = pd.read_csv("/home/pai/Downloads/msml/Eksperimen_SML_Muhammad-nabil-ibrahim/preprocessing/data_clean.csv")
 
 y=df['Heart Disease']
     
@@ -25,35 +19,13 @@ X_train=X_train.drop(columns=['Heart Disease'])
 Y_test=X_test['Heart Disease']
 X_test=X_test.drop(columns=['Heart Disease'])
 
-param_grid = {
-    "C": [0.01, 0.1, 1, 10],
-    "solver": ["liblinear"]
-}
+mlflow.autolog()
 
-grid = GridSearchCV(
-    LogisticRegression(random_state=42),
-    param_grid,
-    cv=3
-)
-
-with mlflow.start_run(run_name="logreg_manual_tuning"):
-    grid.fit(X_train, Y_train)
-    model = grid.best_estimator_
+with mlflow.start_run(run_name="logistic_regression_basic"):
+    model = LogisticRegression(C=0.85,max_iter=1500)
+    model.fit(X_train, Y_train)
 
     y_pred = model.predict(X_test)
-
     acc = accuracy_score(Y_test, y_pred)
-    prec = precision_score(Y_test, y_pred, pos_label="Presence")
-    rec = recall_score(Y_test, y_pred, pos_label="Presence")
 
-    # MANUAL LOGGING
-    mlflow.log_params(grid.best_params_)
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("precision", prec)
-    mlflow.log_metric("recall", rec)
-
-    # ARTEFAK MODEL
-    mlflow.sklearn.log_model(model, "model")
-
-    print("Best params:", grid.best_params_)
-    
+    print("Accuracy:", acc)
